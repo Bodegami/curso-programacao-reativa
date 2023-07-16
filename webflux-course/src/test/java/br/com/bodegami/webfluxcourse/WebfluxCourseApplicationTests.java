e6ea252cca2feb8ef0a5efdb914e4df8f51aece3
Bodegami <rena.f.oliveira@gmail.com>
2023-07-15 23:42:04 -0300

    inicio curso programacao reativa com webflux && setup inicial

---
+++ webflux-course/src/test/java/br/com/bodegami/webfluxcourse/WebfluxCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bodegami.webfluxcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
