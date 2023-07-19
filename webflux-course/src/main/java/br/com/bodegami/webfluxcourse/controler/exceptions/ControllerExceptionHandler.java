c04170f5157ad871184b91fb332f8e36d4355a95
Bodegami <rena.f.oliveira@gmail.com>
2023-07-18 23:19:28 -0300

    Validation error com Jakarta Validation

--- webflux-course/src/main/java/br/com/bodegami/webfluxcourse/controler/exceptions/ControllerExceptionHandler.java
+++ webflux-course/src/main/java/br/com/bodegami/webfluxcourse/controler/exceptions/ControllerExceptionHandler.java
@@ -4,8 +4,10 @@ import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.support.WebExchangeBindException;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
@@ -29,6 +31,26 @@ public class ControllerExceptionHandler {
 
         return ResponseEntity.badRequest().body(response);
     }
+
+    @ExceptionHandler(WebExchangeBindException.class)
+    public ResponseEntity<Mono<ValidationError>> webExchangeBindException(
+            WebExchangeBindException ex, ServerHttpRequest request) {
+
+        ValidationError error = new ValidationError(
+                LocalDateTime.now(),
+                request.getPath().toString(),
+                HttpStatus.BAD_REQUEST.value(),
+                "Validation error",
+                "Error on validation attributes"
+        );
+
+        for (FieldError x : ex.getBindingResult().getFieldErrors()) {
+            error.addError(x.getField(), x.getDefaultMessage());
+        }
+
+        return ResponseEntity.badRequest().body(Mono.just(error));
+    }
+
 
     private String verifyDupKey(String message) {
         if (message.contains("email dup key")) {
